feed344b5a4305ee2a3625b372e7ed486a8981e3
Matthew Phillips <matthew@matthewphillips.info>
2014-05-20 17:14:47 -0400

    Got builder to build configurations for canjs
    Just building the configurations at this point and not correctly
    ignoring the library file, so that is loading.

--- tasks/builder.js
+++ tasks/builder.js
@@ -6,52 +6,59 @@ var pluginify = require("../index").pluginify;
 module.exports = function (grunt) {
 	var _ = grunt.util._;
 
+	/**
+	 * Save out configurations to their final resting place
+	 */
+	function saveConfigurations(options, file, configurations, callback) {
+		var keys = Object.keys(configurations);
+		var dest = file.dest;
+
+		keys.forEach(function(name){
+			var configuration = configurations[name];
+			var library = configuration.library;
+
+			if(configuration.hidden) {
+				return;
+			}
+
+			// The destination for this file
+			var filename = path.join(dest, (options.prefix || "") +
+															 name.toLowerCase() + ".js");
+
+			console.log("Saving...", filename);
+
+			// Get the content for this one.
+			var pluginify = configuration.pluginify;
+			/*var content = pluginify(null, {
+				ignore: library ? [ library ] : []											 
+			});*/
+		 	var content = pluginify();
+
+			grunt.file.write(filename, content);
+		});
+	}
+
+
 	grunt.registerMultiTask("builder", "Pluginify using the download builder configuration", function () {
 		var done = this.async();
 		var options = this.options();
 		var file = this.files[0];
 		options.main = file.src[0].substr(0, file.src[0].length - 3);
 
-		builder(options, function(){
-			console.log("All done");
+		builder(options, function(info){
+			var configurations = info.configurations;
+			var plugins = builder.getPlugins(info.modules);
+
+			// Save out the configuration files
+			saveConfigurations(options, file, configurations, function(){
+
+				// TODO Save out the plugin files
+
+			});
+
 			done();
 		});
 
-		// Process all of the modules one at a time
-		/*function processModules() {
-			var moduleId = keys.shift();
-			// We've finished the last module
-			if(!moduleId) {
-				done();
-			}
-
-			var module = modules[moduleId];
-
-			console.log("Building", module.name);
-
-			var src = file.src[0];
-			var folderPath = path.resolve(fs.realpathSync(src));
-
-			// Pluginify the module
-			pluginify({
-				system: {
-					config: folderPath + "/stealconfig.js",
-					main: "component/component"
-				},
-				exports: {}
-			}).then(function(pluginify){
-				var content = pluginify();
-				
-				var filename = path.resolve(file.dest, module.name.toLowerCase() + ".js");
-				//grunt.file.write(filename, content);
-				
-				processModules();
-			});
-
-		}*/
-
-		//processModules(file);
-		
 	});
 
 };
