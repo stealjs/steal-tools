3a589322bc8b8cfde9eeacce7585526114310328
Justin Meyer <justinbmeyer@gmail.com>
2014-08-12 22:44:26 -0500

    pass data, not options

--- tasks/pluginify.js
+++ tasks/pluginify.js
@@ -4,8 +4,12 @@ module.exports = function(grunt){
 
 	grunt.registerMultiTask("stealPluginify", "Create a 'plugin' version of your project which is not dependent on Steal.", function(){
 		var done = this.async();
-		var options = this.options();
-
+		var options = this.data;
+		["system","outputs"].forEach(function(name){
+			if(!options[name]) {
+				grunt.fail.warn("stealPluginify needs a "+name+" property");
+			}
+		});
 		pluginifierBuilder(options, grunt.config('meta.modules'), grunt.config('meta.defaults'), done);
 	});
 
