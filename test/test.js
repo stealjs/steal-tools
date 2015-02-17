cd8ac332f73a19ff708ed8c0a5fbbf5ddd8e9534
Matthew Phillips <matthew@matthewphillips.info>
2015-02-17 13:41:00 -0500

    Support glob as value for System.bundle
    This allows `System.bundle` to be either an array or a glob. If a glob we'll look in the `baseURL` for files that match the glob pattern and use those as the modules for the bundle option. Fixes #316

--- test/test.js
+++ test/test.js
@@ -138,7 +138,6 @@ describe('dependency graph', function(){
 });
 
 describe("bundle", function(){
-
 	it("should work", function(done){
 
 		bundle({
@@ -155,6 +154,21 @@ describe("bundle", function(){
 		});
 	});
 
+	it("works with globs", function(done){
+		bundle({
+			config: __dirname+"/bundle/stealconfig.js",
+			main: "bundle",
+			logLevel: 3,
+			bundle: "app_*"
+		}).then(function(data){
+			var graphCompare = require('./bundle/bundle_graph');
+			comparify(data.graph, graphCompare, true);
+			done();
+
+		}).catch(function(e){
+			done(e)
+		});
+	});
 });
 
 
