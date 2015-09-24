c1b314f713a69fcbaf4b3cc475d64b341ca56c3d
Matthew Phillips <matthew@matthewphillips.info>
2015-09-24 08:16:37 -0400

    Fixes race condition in live-reload server initialization.
    This is related to https://github.com/donejs/donejs/issues/94
    
    When can-serve and steal-tools live-reload are started at the same time with `donejs develop` it causes a race condition where if can-server loads before live-reload it will send a ping to the live-reload server to load the `System.main` into the stream. However it is possible that the graph hasn't been loaded by live-reload at this point, and if so an error will occur. The fix is to create a Promise out of the `graphStream` so that we don't write into it until it has been initiated.

--- test/test_live.js
+++ test/test_live.js
@@ -2,23 +2,24 @@ var assert = require("assert");
 var live = require("../lib/stream/live");
 var fs = require("fs");
 var asap = require("pdenodeify");
+var WebSocket = require("ws");
 
 describe("live-reload", function(){
 	this.timeout(10000);
 
 	var fooPath = __dirname + "/live_reload/foo.js";
 
-	beforeEach(function(done){
+	before(function(done){
 		var self = this;
 		asap(fs.readFile)(fooPath, "utf8").then(function(content){
 			self._fooModule = content;
 		}).then(done, done);
 	});
 
-	afterEach(function(done){
+	after(function(done){
 		asap(fs.writeFile)(fooPath, this._fooModule, "utf8").then(function(){
 			done();
-		}, done);
+		});
 	});
 
 	it("Starts a web socket server", function(done){
@@ -26,16 +27,24 @@ describe("live-reload", function(){
 			config: __dirname + "/live_reload/package.json!npm"
 		}, {});
 
-		liveStream.once("data", function(data){
-			assert(/bar/.test(data.graph.foo.load.source),
-				   "Initial source contains 'bar'");
+		var ws = new WebSocket("ws://localhost:8012");
 
-			var newSource = "module.exports = 'foo';";
-			asap(fs.writeFile)(fooPath, newSource, "utf8").then(function(){
-				liveStream.once("data", function(data){
-					assert(/foo/.test(data.graph.foo.load.source),
-						   "New source contains 'foo'");
-					done();
+		ws.on("open", function(){
+			ws.send("main");
+		});
+
+		liveStream.once("data", function(data){
+			liveStream.once("data", function(data){
+				assert(/bar/.test(data.graph.foo.load.source),
+					   "Initial source contains 'bar'");
+
+				var newSource = "module.exports = 'foo';";
+				asap(fs.writeFile)(fooPath, newSource, "utf8").then(function(){
+					liveStream.once("data", function(data){
+						assert(/foo/.test(data.graph.foo.load.source),
+							"New source contains 'foo'");
+						done();
+					});
 				});
 			});
 		});
@@ -43,5 +52,4 @@ describe("live-reload", function(){
 			done(err);
 		});
 	});
-
 });
