2191f3aff80b681e52b67746a61e1c757349a664
Matthew Phillips <matthew@matthewphillips.info>
2015-09-30 10:09:20 -0400

    Detect changes to dynamic bundles in graph/recycle
    The done-autorender plugin and the can/view/stache/system plugin will both detect dynamic imports and add those to the `bundle` array on the loader. When recycling a module (such as in live-reload) we have an optimization that only reloads the dependency graph when there have been changes to the module's static dependencies. We need to compare the `bundle` array as well, and if that has changed also reload the dependency graph. Fixes #313

--- test/test.js
+++ test/test.js
@@ -253,6 +253,51 @@ if(!isIOjs) {
 			depStream.write(config.main);
 
 		});
+
+		it("Detects dynamic imports added when no static dependencies have changed", function(done){
+			var config = {
+				config: path.join(__dirname, "/recycle_dynamic/config.js"),
+				main: "something.txt!plug",
+				logLevel: 3,
+				map: { "@dev": "@empty" }
+			};
+
+			var depStream = bundle.createBundleGraphStream(config);
+			var recycleStream = recycle(config);
+
+			depStream.pipe(recycleStream);
+
+			// Wait for it to initially finish loading.
+			recycleStream.once("data", function(data){
+				var node = data.graph["something.txt!plug"];
+
+				// Update the module so that it has a dynamic import. This should
+				// be added to the loader's bundle and the graph reloaded.
+				var mockOptions = {};
+				Object.keys(data.graph).forEach(function(moduleName){
+					var load = data.graph[moduleName].load;
+					mockOptions[load.address.replace("file:", "")] = load.source;
+				});
+				mockOptions[node.load.address.replace("file:", "")] =
+					'System.import("another");';
+				mockOptions[path.resolve(__dirname + "/recycle_dynamic/another.js")]
+					= 'var dep = require("./dep");';
+				mockOptions[path.resolve(__dirname + "/recycle_dynamic/dep.js")]
+					= 'module.exports = "dep";';
+				mockFs(mockOptions);
+
+				recycleStream.write(node.load.name);
+				recycleStream.once("data", function(data){
+					var graph = data.graph;
+					assert(graph["another"], "this bundle was added to the graph");
+					assert(graph["dep"], "the bundle's dependency was also added");
+					done();
+				});
+			});
+
+			depStream.write(config.main);
+
+		});
 	});
 }
 
