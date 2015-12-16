830b7fb6de130a9fc878c353f922632503fca8c2
Matthew Phillips <matthew@matthewphillips.info>
2015-12-16 09:15:21 -0500

    Deferring this change for now

--- test/test.js
+++ test/test.js
@@ -2354,7 +2354,7 @@ describe("npm package.json builds", function(){
 		});
 	};
 
-	it("only needs a config", function(done){
+	it.only("only needs a config", function(done){
 		this.timeout(50000);
 		setup(function(error){
 			if(error){ return done(error); }
