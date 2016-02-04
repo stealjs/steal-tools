404557c344f5747c97aa19e5f75fbc3b27437966
Matthew Phillips <matthew@matthewphillips.info>
2016-02-04 09:39:49 -0500

    Reuse some config when loading bundles
    This fixes #365. Some configuration might need to be reused when loading multiple bundles in multiple loaders. With the new progressive loading of package.jsons we need to keep the `npmContext` between loaders because each bundle will have a different set of NPM dependencies, and we need to build them **all** in the final output.
    
    This will also require a fix in system-npm to reuse an existing npmContext.

---
+++ test/progressive_package/main.js
@@ -0,0 +1,14 @@
+require("dep1");
+
+System.import("bundleA").then(function(a){
+	System.import("bundleB").then(function(b){
+		window.MODULE = {
+			a: a,
+			b: b
+		};
+
+		if(System.isEnv("development")) {
+			console.log("bundles loaded", window.MODULE);
+		}
+	});
+});
