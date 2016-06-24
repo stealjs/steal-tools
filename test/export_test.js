d51fcf621bcff3c6a4a1eda192a8f5060d4f794b
Matthew Phillips <matthew@matthewphillips.info>
2016-06-24 08:49:45 -0400

    Better global exports for ES modules
    This makes the global export (the one that is put on the window) nicer for ES modules. It makes it so that:
    
    * If there are multiple named exports, the Module is placed on the global property.
    * If there is only a single default export, the "default" is placed on the global property.
    
    Closes #480

--- test/export_test.js
+++ test/export_test.js
@@ -378,39 +378,6 @@ describe("export", function(){
 			}, done);
 		});
 
-		describe("+global-css", function(){
-			it("Builds even if there is no css", function(done){
-				rmdir(__dirname + "/exports_basics/dist", function(err){
-					if(err) return done(err);
-
-					stealExport({
-						system: {
-							config: __dirname + "/exports_basics/package.json!npm",
-							main: "app/main-no-css"
-						},
-						options: { quiet: true },
-						outputs: {
-							"+global-css": {},
-							"+global-js": {
-								exports: {
-									"app/main-no-css": "MOD"
-								}
-							}
-						}
-					}).then(function(){
-						open("test/exports_basics/prod-no-css.html",
-							function(browser, close) {
-							find(browser, "MOD", function(mod){
-								assert.equal(mod.foo, "this is a blank main",
-											 "did export the js");
-								close();
-							}, close);
-						}, done);
-					});
-				});
-			});
-		});
-
 		it("+cjs +amd +global-css +global-js using Babel", function(done){
 			this.timeout(10000);
 			stealExport({
@@ -462,36 +429,6 @@ describe("export", function(){
 					}, close);
 				}, done);
 			}, done);
-		});
-
-		describe("+standalone", function(){
-			it("Works with exporting a module from a dependency", function(done){
-				this.timeout(10000);
-				stealExport({
-					system: {
-						config: __dirname+"/exports_basics/package.json!npm"
-					},
-					options: { quiet: true },
-					"outputs": {
-						"+standalone": {
-							exports: {
-								"foo": "FOO.foo"
-							},
-							dest: __dirname + "/exports_basics/out.js"
-						}
-					}
-				})
-				.then(function() {
-					open("test/exports_basics/global.html",
-						 function(browser, close) {
-						find(browser,"FOO", function(foo){
-							assert.equal(foo.foo.bar.name, "bar", "it worked");
-							close();
-						}, close);
-					}, done);
-				}, done);
-
-			});
 		});
 
 	});
