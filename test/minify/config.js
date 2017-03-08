b0bc5f4bbef6c297119146f3528bfdb15138a4fd
Manuel Mujica <manuel.mujica.rojas@gmail.com>
2017-03-08 11:49:21 -0700

    Adjust code to workaround UglifyJS dead code removal

--- test/minify/config.js
+++ test/minify/config.js
@@ -1,8 +1,14 @@
 // just to make sure this file is being minified properly
-var foobar = (function() {
-	var anotherLongVariableName = "foo";
-	var baz = "baz";
-	return anotherLongVariableName + baz;
-}());
+
+// this code is a noop meant to force UglifyJS to include the
+// `anotherLongVariableName` (with the mangle flag off) in the minified code
+// for testing purposes
+var anotherLongVariableName;
+
+function funcName(firstLongName, lastLongName) {
+  anotherLongVariableName = firstLongName +  lastLongName;
+}
+
+if (anotherLongVariableName == "foo") { console.log(); };
 
 System.paths.foo = "bar";
