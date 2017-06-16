358bfa109514a3e414444fc83f9eb85d9cddc98b
Manuel Mujica <manuel.mujica.rojas@gmail.com>
2017-06-16 08:30:52 -0600

    Split slim support checks into individual files

--- test/test.js
+++ test/test.js
@@ -16,7 +16,7 @@ require("./cli/make_outputs_test");
 require("./cli/cmd_live_test");
 require("./get_es_module_imports_test");
 require("./cli/make_build_options_test");
-require("./check_slim_graph_support_test");
+require("./slim_support_checks_test");
 
 // Integration tests
 require("./test_cli");
