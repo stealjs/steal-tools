d57e5cbea6abd725a27b1f39aacf851da7f7f533
Manuel Mujica <manuel.mujica.rojas@gmail.com>
2017-06-30 17:19:38 -0600

    Add file size benchmarks
    Closes #664

---
+++ test/check_size_snapshot.js
@@ -0,0 +1,62 @@
+var path = require("path");
+var fs = require("fs-extra");
+var colors = require("colors");
+var gzipSize = require("gzip-size");
+var denodeify = require("pdenodeify");
+
+var readFile = denodeify(fs.readFile);
+var writeFile = denodeify(fs.writeFile);
+
+/**
+ * Validates bundle size snapshot or creates it if missing
+ * @param {string} bundle - The path to the bundle to be checked
+ * @param {string} dest - The path to the folder where the snapshot is located
+ * @return {Promise.<number>} A promise that resolves if:
+ *		a) No previous snapshot found, writes the file and resolves the gzip size
+ *		b) The bundle size is the same as the previous snapshot, resolves size
+ *		c) The bundle size is smaller than the snapshot, updates snapshot, resolves size
+ */
+module.exports = function(bundle, dest) {
+	var snapPath = path.join(dest, `${path.basename(bundle, ".js")}-size.snap`);
+
+	return new Promise(function(resolve, reject) {
+		readFile(bundle)
+			.then(function(data) {
+				return Promise.all([ data.toString(), safeRequire(snapPath) ]);
+			})
+			// [ bundle : String, snapshot : Number ]
+			.then(function(data) {
+				var oldSize = data[1];
+				var newSize = gzipSize.sync(data[0]);
+
+				// just resolve if the bundle size did not change
+				if (oldSize === newSize) {
+					return resolve(newSize);
+				}
+
+				// reject promise if size has increased
+				if (oldSize != null && newSize > oldSize) {
+					return reject(
+						new Error(`Bundle size has increased, before: ${oldSize} now: ${newSize}`)
+					);
+				}
+
+				// write the snapshot for the first time or if bundle size smaller
+				return writeFile(snapPath, `module.exports = ${newSize};`)
+					.then(function() {
+						console.log(colors.yellow.underline(`\tPlease commit ${path.basename(snapPath)}`));
+						return newSize;
+					});
+			})
+			.then(resolve)
+			.catch(reject);
+	});
+};
+
+function safeRequire(snapshot) {
+	try {
+		return require(snapshot);
+	} catch (e) {
+		return null;
+	}
+}
