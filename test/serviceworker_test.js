04d420747be6c576bb5551f932d81860dc0b5a32
Julian Kern <kern.julian.86@googlemail.com>
2017-09-24 19:38:40 +0200

    simple serviceworker test

--- test/serviceworker_test.js
+++ test/serviceworker_test.js
@@ -1,17 +1,9 @@
 var asap = require("pdenodeify"),
 	assert = require("assert"),
-	comparify = require("comparify"),
 	fs = require("fs-extra"),
 	multiBuild = require("../lib/build/multi"),
 	rmdir = require("rimraf"),
-	path = require("path"),
-	stealTools = require("../index"),
-	testHelpers = require("./helpers");
-
-var precache = require("steal-serviceworker");
-
-var find = testHelpers.find;
-var open = testHelpers.open;
+	path = require("path");
 
 describe("serviceworker", function () {
 	this.timeout(5000);
@@ -29,13 +21,21 @@ describe("serviceworker", function () {
 		multiBuild({
 			config: __dirname + "/serviceworker/package.json!npm"
 		}, {
-			quiet: false,
-			serviceWorker: true,
-			bundleAssets: true
+			quiet: true,
+			minify: false,
+			serviceWorker: true
 		}).then(function (builtResult) {
+
+			// a valid buildResult?
+			assert.equal(builtResult.mains[0], "serviceworker@0.0.1#index");
+			assert.ok(builtResult.hasOwnProperty("configuration"));
+			assert.ok(builtResult.hasOwnProperty("graph"));
+			assert.ok(builtResult.hasOwnProperty("loader"));
 
 			assert.ok(fs.existsSync(path.join(__dirname, "serviceworker", "service-worker.js")));
 			done();
 		}).catch(done);
 	});
+
+	// for more tests, see https://github.com/stealjs/steal-serviceworker/blob/master/test/test.js
 });
