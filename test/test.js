18259e5cb791c3edeee78ba4b92218dc17e72720
Manuel Mujica <manuel.expat@gmail.com>
2019-11-17 19:52:12 -0300

    Rollback main test file changes
    I did not consider it would break the test coverage scripts

--- test/test.js
+++ test/test.js
@@ -1,72 +1,51 @@
-var Mocha = require("mocha");
-var path = require("path");
+require("./recycle_test");
 
-var mocha = new Mocha();
+// Unit tests
+require("./clean_address_test");
+require("./clean_test");
+require("../lib/bundle/bundle_test");
+require("./cli/cmd_build_test");
+require("./cli/cmd_bundle_test");
+require("./cli/cmd_build_int_test");
+require("./cli/cmd_transform_test");
+require("./cli/make_steal_config_test");
+require("./cli/cmd_export_test");
+require("./cli/make_outputs_test");
+require("./cli/cmd_live_test");
+require("./get_es_module_imports_test");
+require("./cli/make_build_options_test");
+require("./slim_build_conditionals_test");
+require("./minify_js_test");
+require("./tree_shaking_test");
 
-var testFiles = [
-	"./recycle_test",
+// Integration tests
+require("./test_cli");
+require("./test_live");
+require("./bundle_name_test");
+require("./dependencygraph_test");
+require("./bundle_test");
+require("./order_test");
 
-	// Unit tests
-	"./clean_address_test",
-	"./clean_test",
-	"../lib/bundle/bundle_test",
-	"./cli/cmd_build_test",
-	"./cli/cmd_bundle_test",
-	"./cli/cmd_build_int_test",
-	"./cli/cmd_transform_test",
-	"./cli/make_steal_config_test",
-	"./cli/cmd_export_test",
-	"./cli/make_outputs_test",
-	"./cli/cmd_live_test",
-	"./get_es_module_imports_test",
-	"./cli/make_build_options_test",
-	"./slim_build_conditionals_test",
-	"./minify_js_test",
-	"./tree_shaking_test",
+require("./multibuild_test");
+require("./multi_main_build_test");
+require("./transform_test");
+require("./export_test");
+require("./export_global_js_test");
+require("./export_global_css_test");
+require("./export_standalone_test");
+require("./export_bundled_es_test");
+require("./continuous_test");
+require("./concat_test");
+require("./graph_stream_test");
+require("./transpile_test");
+require("./write_stream_test");
+require("./build_conditionals_test");
+require("./dev_bundle_build_test");
+require("./babel_presets_test");
+require("./babel_plugins_test");
+require("./slim_build_test");
+require("./slim_loader_size_test");
 
-	// Integration tests
-	"./test_cli",
-	"./test_live",
-	"./bundle_name_test",
-	"./dependencygraph_test",
-	"./bundle_test",
-	"./order_test",
-
-	"./multibuild_test",
-	"./multi_main_build_test",
-	"./transform_test",
-	"./export_test",
-	"./export_global_js_test",
-	"./export_global_css_test",
-	"./export_standalone_test",
-	"./export_bundled_es_test",
-	"./continuous_test",
-	"./concat_test",
-	"./graph_stream_test",
-	"./transpile_test",
-	"./write_stream_test",
-	"./build_conditionals_test",
-	"./dev_bundle_build_test",
-	"./babel_presets_test",
-	"./babel_plugins_test",
-	"./slim_build_test",
-	"./slim_loader_size_test",
-
-	// external steal-tools plugins
-	"./bundle_assets_test"
-	//"./serviceworker_test", skip for now
-];
-
-testFiles.forEach(function(file) {
-	mocha.addFile(path.join(__dirname, file + ".js"));
-});
-
-try {
-	mocha.run(function(failures) {
-		// exit with non-zero status if there were failures
-		process.exit(failures ? 1 : 0);
-	});
-} catch (error) {
-	console.error(error);
-	process.exit(1);
-}
+// external steal-tools plugins
+require("./bundle_assets_test");
+//require("./serviceworker_test"); skip for now
